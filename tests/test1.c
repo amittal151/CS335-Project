222356e8a90cc7c3a7dbfdf843ec0994dca1780a
bhubam <bamoriyab61@gmail.com>
2021-02-07 19:08:08 +0530

    test1.c changed

--- tests/test1.c
+++ tests/test1.c
@@ -1,9 +1,6 @@
-int main () {
-    // read_input_file();
-    printf("\nPlease enter Source(s) and Sink(t) :\n");
-    int s=0;
-    int t=n-1;
-    scanf("%d %d",&s,&t);
-    printf("\nMax Flow : %d\n",max_flow(s,t));
-    return 0;
+int main() {
+	int a = 1;
+	int b = *a+2;
+	return 0;
 }
+
