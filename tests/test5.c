a223a29a6e3aadeb16edd381a80664a6ad45c56b
amittal151 <abhimittal151@gmail.com>
2021-02-08 18:57:54 +0530

    Removed errors

---
+++ tests/test5.c
@@ -0,0 +1,7 @@
+int main(){
+    int token = 123;
+    /* abssfhskdlfnlsdf****\**eiwbnewqwe\nsehew/kds@#$%^&!nksd/**
+    /\nhriawe*/ 
+    return 0;
+    fndsljfndr
+}
---
+++ tests/test6.c
@@ -0,0 +1,12 @@
+x = 300; // Assigning \n x=3 \n
+/* a simple comment */
+/* also /* a comment */
+/* **a comment * with a silly character* in it */
+/* a multi-line
+		comment
+	*/
+/* two comments on \n\n \n one line /a->value =1.12;/ NOT one long comment */
+/* a comment with a silly character at the end **/
+z == /*complicated comment 1;
+in between
+some operation*/ 'a';
