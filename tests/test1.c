484b716c728bcc7b363cf0d73ad0531abede016e
bhubam <bamoriyab61@gmail.com>
2021-02-08 18:23:17 +0530

    test1.c changed

--- tests/test1.c
+++ tests/test1.c
@@ -1,6 +1,18 @@
-int main() {
-	int a = 1;
+int main() 
+{ 
+   int x = 2; 
+   int a = 1;
 	int b = *a+2;
-	return 0;
-}
-
+   switch (x) 
+   { 
+       case 1: printf("Choice is 1"); 
+               break; 
+       case 2: printf("Choice is 2"); 
+                break; 
+       case 3: printf("Choice is 3"); 
+               break; 
+       default: printf("Choice other than 1, 2 and 3"); 
+                break;   
+   } 
+   return 0; 
+}  
